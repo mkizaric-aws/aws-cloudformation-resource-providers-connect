c7178293c7b0b83ff120a132585c31aca251b37a
MattKizaric <mkizaric@amazon.com>
2024-08-02 11:46:12 -0700

    Initializing cfn project

---
+++ aws-connect-contactflow-version/src/main/java/software/amazon/connect/contactflowversion/Configuration.java
@@ -0,0 +1,8 @@
+package software.amazon.connect.contactflowversion;
+
+class Configuration extends BaseConfiguration {
+
+    public Configuration() {
+        super("aws-connect-contactflowversion.json");
+    }
+}
